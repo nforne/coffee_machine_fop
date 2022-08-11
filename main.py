7ea8c0288f4727dc2ff29f06bce1e3201ec38995
nforne <mnforne@gmail.com>
2022-08-11 17:59:07 -0400

    cli coffee machine vending machine simulator

---
+++ main.py
@@ -0,0 +1,22 @@
+from resources_and__ import *
+from art import logo
+from replit import clear
+
+is_on = True
+while is_on:
+    clear()
+    print(logo)
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
